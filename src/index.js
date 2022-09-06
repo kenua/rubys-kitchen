d01dd9a9dd5256ae8b205663945dfc25280c0398
Kenua <jgvezon@gmail.com>
2022-09-06 07:43:28 -0500

    Update colors and active navbar item
    Changed $primary and $secondary to be more darker and updated index.js
    to highlight a navbar__anchor based on currentSectionName value.

--- src/index.js
+++ src/index.js
@@ -5,12 +5,14 @@ import "./sass/styles.scss";
 
 document.addEventListener("DOMContentLoaded", () => {
    const navbarUl = document.getElementById("navbar-ul");
+   const navbarAnchors = [...document.getElementsByClassName("navbar__anchor")];
 
    let currentSectionNode;
    let currentSectionName;
 
    currentSectionNode = insertHome();
    currentSectionName = "home";
+   navbarAnchors[0].className = "navbar__anchor navbar__anchor--active";
 
    navbarUl.addEventListener("click", changeSection);
 
@@ -38,5 +40,12 @@ document.addEventListener("DOMContentLoaded", () => {
             currentSectionName = "about";
             break;
       }
+
+      navbarAnchors.forEach((anchor) => (anchor.className = "navbar__anchor"));
+      navbarAnchors.forEach((anchor) => {
+         if (anchor.getAttribute("href") === currentSectionName) {
+            anchor.className = "navbar__anchor navbar__anchor--active";
+         }
+      });
    }
 });
