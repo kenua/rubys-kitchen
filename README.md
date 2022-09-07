7e637cb4f3585ec19771e3e78f4ef990ce76effd
Kenua <jgvezon@gmail.com>
2022-09-07 06:46:49 -0500

    Add docs folder
    Replace dist/ folder with docs/ and added a webpack configuration file
    for production, a readme file and a screenshot.

---
+++ README.md
@@ -0,0 +1,6 @@
+# Restaurant Page
+<img src="./restaurant-screenshot.png" alt="Restaurant Page Screenshot" width="800">
+
+A static page build with HTML5, Sass, Javascript, and bundled with Webpack.
+
+<s href="#" target="_blank">Live Preview</a>
--- webpackProd.config.js
+++ webpackProd.config.js
@@ -2,16 +2,12 @@ const path = require("path");
 const HtmlWebpackPlugin = require("html-webpack-plugin");
 
 module.exports = {
-   mode: "development",
+   mode: "production",
    entry: "./src/index.js",
    output: {
       filename: "main.js",
-      path: path.resolve(__dirname, "build"),
+      path: path.resolve(__dirname, "docs"),
       clean: true,
-   },
-   devtool: "inline-source-map",
-   devServer: {
-      static: "./build",
    },
    plugins: [
       new HtmlWebpackPlugin({
